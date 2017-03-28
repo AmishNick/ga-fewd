af14277243121508a75fa1320d4f10673a1b7d94
AmishNick <nicholas.moroz@gmial.com>
2017-03-28 18:32:31 -0400

    day 11 work and lab

--- d11/materials/temperature-converter/js/project.js
+++ d11/materials/temperature-converter/js/project.js
@@ -1,11 +1,15 @@
 var cText;
 var tempC;
 var tempF;
+var CorF = $("select").val();
 
 $("button").on("click", function() {
   //cText = document.getElementById("temperature").value;
-  cText = $("input").val();
-  tempC = parseInt(cText);
-  tempF = tempC * 9 / 5 + 32;
-  $("span#converted").text(tempF);
+   if (CorF === "Celsius") 
+   {  
+     cText = $("input").val();
+     tempC = parseInt(cText);
+     tempF = tempC * 9 / 5 + 32;
+     $("span#converted").text(tempF);
+    }
 })
